bb490bf62c4016f0805710b2be7cd8564e84fb47
Rosalma Wehbe López <rosalmawehbelopez@macbook-pro-de-rosalma.local>
2016-09-29 12:28:31 +0200

    Initial Commit

---
+++ Prac 2.2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prac 2.2
+//
+//  Created by Rosalma Wehbe López on 29/9/16.
+//  Copyright © 2016 upm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
